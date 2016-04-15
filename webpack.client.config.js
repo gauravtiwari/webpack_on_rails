033dbae2771453eb58ddd5191cc7fbe04a2975aa
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-15 09:43:00 +0200

    Refactor webpack config

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,54 +1,75 @@
-// Run like this:
-// npm run build:dev:client
-// Note that Foreman (Procfile.dev) has also been configured to take care of this.
-
-// NOTE: All others assets are handles by rails asset pipeline,
-// We use webpack just for bundling the deps and our app for browser
-
+// Base client-side webpack configuration
 const webpack = require('webpack');
-const config = require('./webpack.client.base.config');
+const path = require('path');
+
 const devBuild = process.env.NODE_ENV !== 'production';
+const nodeEnv = devBuild ? 'development' : 'production';
 
-// Outputs the app-bundle.js into /webpack folder under assets
-config.output = {
-  filename: '[name]-bundle.js',
-  path: './app/assets/webpack',
+module.exports = {
+  // the project dir
+  context: __dirname,
+  entry: {
+    // See use of 'vendor' in the CommonsChunkPlugin inclusion below.
+    vendor: [
+      'babel-polyfill',
+      'jquery',
+      'jquery-ujs',
+      'react-dom',
+      'react-dom/server',
+      'react',
+      'es5-shim/es5-shim',
+      'es5-shim/es5-sham',
+    ],
+    // Main component entry file: components.jsx
+    app: [
+      './app/assets/javascripts/components',
+    ],
+  },
+  output: {
+    filename: '[name]-bundle.js',
+    path: './app/assets/webpack',
+  },
+  // Extensions to resolve
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.es6.js'],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(nodeEnv),
+      },
+    }),
+    // https://webpack.github.io/docs/list-of-plugins.html#2-explicit-vendor-chunk
+    new webpack.optimize.CommonsChunkPlugin({
+
+      // This name 'vendor' ties into the entry definition
+      name: 'vendor',
+      filename: 'vendor-bundle.js',
+
+      // Passing Infinity just creates the commons chunk, but moves no modules into it.
+      // In other words, we only put what's in the vendor entry definition in vendor-bundle.js
+      minChunks: Infinity,
+    }),
+  ],
+  module: {
+    loaders: [
+
+      // For react-rails we need to expose these deps to global object
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      { test: require.resolve('jquery'), loader: 'expose?jQuery' },
+      { test: require.resolve('jquery'), loader: 'expose?$' },
+      {
+        test: require.resolve('react'),
+        loader: 'imports?shim=es5-shim/es5-shim&sham=es5-shim/es5-sham',
+      },
+      { test: require.resolve('react'), loader: 'expose?React' },
+      { test: require.resolve('react-dom'), loader: 'expose?ReactDOM' },
+      { test: require.resolve('react-dom/server'), loader: 'expose?ReactDOMServer' }
+    ],
+  },
 };
-
-// The es5-shim/sham is for capybara testing
-config.entry.vendor.unshift(
-  'es5-shim/es5-shim',
-  'es5-shim/es5-sham'
-);
-
-// jquery-ujs depends on jquery so much be
-config.entry.vendor.push('jquery-ujs');
-
-// See webpack.common.config for adding modules common to both the webpack dev server and rails
-config.module.loaders.push(
-  {
-    test: /\.jsx?$/,
-    loader: 'babel-loader',
-    exclude: /node_modules/,
-  },
-  {
-    test: require.resolve('react'),
-    loader: 'imports?shim=es5-shim/es5-shim&sham=es5-shim/es5-sham',
-  },
-  {
-    test: require.resolve('jquery-ujs'),
-    loader: 'imports?jQuery=jquery',
-  }
-);
-
-module.exports = config;
-
-if (devBuild) {
-  console.log('Webpack dev build for Rails'); // eslint-disable-line no-console
-  module.exports.devtool = 'eval-source-map';
-} else {
-  config.plugins.push(
-    new webpack.optimize.DedupePlugin()
-  );
-  console.log('Webpack production build for Rails'); // eslint-disable-line no-console
-}
