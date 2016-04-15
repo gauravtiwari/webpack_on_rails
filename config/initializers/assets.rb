9871dd44c1e4c71585336ded2e2ac38a9af55126
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-15 09:13:09 +0200

    Add webpack assets to precompile

---
+++ config/initializers/assets.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+Rails.application.config.assets.precompile += %w( server-bundle.js )
+Rails.application.config.assets.precompile += %w( server_rendering.js)
+# Add folder with webpack generated assets to assets.paths
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "webpack")
