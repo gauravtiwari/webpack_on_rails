7da03eaa99d0c1997800e81cba6d09612bbc4bc3
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-15 10:00:12 +0200

    Annotate webpack server config

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -10,22 +10,27 @@ module.exports = {
   // the project dir
   context: __dirname,
 
+  // Vendor and entry point of the app
   entry: [
     'babel-polyfill',
     'react-dom/server',
     'react',
     './app/assets/javascripts/components',
   ],
+  // Bundled output path
   output: {
     filename: 'server-bundle.js',
     path: './app/assets/webpack',
   },
+
+  // Extensions to resolve
   resolve: {
     extensions: ['', '.js', '.jsx', '.es6.js'],
     alias: {
       lib: path.join(process.cwd(), 'app', 'lib'),
     },
   },
+  // Add plugins
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
@@ -35,6 +40,8 @@ module.exports = {
   ],
   module: {
     loaders: [
+      // react-rails would need certain global objects plus we need to use babel loader
+      // for ES6 code
       { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: require.resolve('jquery'), loader: 'expose?jQuery' },
       { test: require.resolve('jquery'), loader: 'expose?$' },
